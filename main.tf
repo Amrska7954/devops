46a30b1ff19f389ab73b4b6a47cbe8280ce6ffb2
Amarkadam <amarkadam7954.ak@gmail.com>
2023-09-05 17:53:30 -0600

    code for s3

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+profile = "myprofile"
+}
+
+resource "aws_s3_bucket" "example_amamr1009" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+# resource "aws_instance" "example_instance" {
+#   ami           = "ami-08a52ddb321b32a8c"          # Replace with the actual AMI ID
+#   instance_type = "t2.micro"
+#   key_name      = "YOUR_KEY_PAIR_NAME"
+
+#   tags = {
+#     Name = "ExampleInstance"
+#   }
+# }
