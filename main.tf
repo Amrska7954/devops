77f7adc93e7c7fdc4c2127215088aa9f920a104b
Amarkadam <amarkadam7954.ak@gmail.com>
2023-09-06 15:58:35 -0600

    5th subnet

--- main.tf
+++ main.tf
@@ -54,10 +54,9 @@ resource "aws_vpc" "main" {
 
 #code 4
 
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
- 
- tags = {
-   Name = "Project VPC"
- }
-}
+# resource "aws_vpc" "main" {
+#  cidr_block = "10.0.0.0/16"
+#  tags = {
+#    Name = "Project VPC"
+#  }
+# }
