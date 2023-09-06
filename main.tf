cb5924deaea933eb5781604c4a0ad1ed234acba4
Amarkadam <amarkadam7954.ak@gmail.com>
2023-09-06 15:56:44 -0600

    4th commit

--- main.tf
+++ main.tf
@@ -42,3 +42,22 @@ module "vpc" {
 }
 
 #code 3
+
+
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
+}
+
+#code 4
+
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
+}
